774fe0f07c4f542dbcbeaa8d6cf017579790bdfb
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2020-10-18 14:04:18 +0700

    Add files via upload

---
+++ tantawan/pokedeng/Deck.cs
@@ -0,0 +1,57 @@
+﻿using System;                                                                                                                                                                      
+
+namespace Tan.card
+{
+	public class Deck
+	{
+		private readonly int[] cards;
+		private int nextCard;
+		private readonly Random random;
+
+		public Deck()
+		{
+			random = new Random();
+			cards = new int[52];
+			Initialize();
+		}
+
+		public void Initialize()
+		{
+			for (int i = 0; i < cards.Length; i++)
+			{
+				cards[i] = i;
+			}
+			nextCard = cards.Length;
+			Shuffle();
+		}
+
+		public bool Shuffle(int times = 60)
+		{
+			if (nextCard != cards.Length)
+			{
+				return false;
+			}
+			int j, k, holder;
+			for (int i = 0; i < times; i++)
+			{
+				j = random.Next(52); 
+				k = random.Next(52);
+				holder = cards[j];
+				cards[j] = cards[k];
+				cards[k] = holder;
+
+			}
+			return true;
+		}
+		public Card Deal()
+		{
+			nextCard--;
+			if (nextCard < 0)
+			{
+				return null;
+			}
+			return new Card(cards[nextCard]);
+		}
+	}
+}
+
---
+++ tantawan/tantawan/program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tan.card;
+
+namespace tantawan
+{
+    class program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Deck deck = new Deck();
+            Pokedeng gamePlay = new Pokedeng("dealer", deck);
+            Player mook = new Player("mook");
+            gamePlay.AddPlayer(mook);
+            gamePlay.AddPlayer(new Player("Tan"));
+            gamePlay.AddPlayer(new Player("Ton"));
+            gamePlay.Play();
+
+            
+
+        }
+    }
+}
